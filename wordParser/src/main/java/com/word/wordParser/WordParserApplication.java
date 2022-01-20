9e489c50872484f6abf59608e547e51c6624f4de
singamaneni-gowtham <gowtham.singamaneni@gmail.com>
2022-01-20 11:15:51 +0530

    this is my first commit

---
+++ wordParser/src/main/java/com/word/wordParser/WordParserApplication.java
@@ -0,0 +1,13 @@
+package com.word.wordParser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordParserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordParserApplication.class, args);
+	}
+
+}
---
+++ wordParser/src/test/java/com/word/wordParser/WordParserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.word.wordParser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordParserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
